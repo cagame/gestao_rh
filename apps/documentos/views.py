a9c348264107416ca4bcb16577abd024d809d103
cagame <gabriel_vixbr@hotmail.com>
2019-06-03 18:05:38 -0300

    subindo para amazon

--- apps/documentos/views.py
+++ apps/documentos/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from apps.documentos.models import Documento
 
-# Create your views here.
+
+class DocumentCreate(CreateView):
+    model = Documento
+    fields = ['descricao', 'arquivo']
+
+    def post(self, request, *args, **kwargs):
+        form = self.get_form()
+        form.instance.funcionario_id = self.kwargs['funcionario_id']
+
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
